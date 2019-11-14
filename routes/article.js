c48f83e50357568ce4b750eadcf738ffa9a7d2cd
Misheck Githongo <misheckgithongo@gmail.com>
2019-11-14 23:47:58 +0300

    implemented patch/article/<:articleId> endpoint

--- routes/article.js
+++ routes/article.js
@@ -6,5 +6,6 @@ const articleCtrl = require('../controllers/article')
 
 
 router.post('/articles', articleCtrl.createArticle)
+router.patch('/articles/:articleId', articleCtrl.editArticle)
 
 module.exports = router
