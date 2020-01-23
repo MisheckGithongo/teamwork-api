f3c01a904742e6dd9afac6c1abd86bdd582bb982
Misheck Githongo <misheckgithongo@gmail.com>
2020-01-24 00:24:43 +0300

    set cors allow credentials to true

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const app = express()
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
+  response.setHeader("Access-Control-Allow-Credentials", "true")
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
   next()
